3eed824b53cc3d3e0eab4578203f29ab00e7bb54
Stephen Sauceda <contact@stephensauceda.com>
2016-10-06 21:34:33 -0400

    db stuff

--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import makeHash from './lib/makeHash'
 
+import firebase from 'firebase'
+
+const config = {
+  apiKey: process.env.apikey,
+  authDomain: process.env.authdomain,
+  databaseURL: process.env.databaseurl,
+  storageBucket: process.env.storagebucket,
+  messagingSenderId: process.env.messagingsenderid
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database()
+
 export const app = express()
 
 app.use(bodyParser.json())
@@ -11,7 +25,21 @@ app.get('/', (req, res) => {
 })
 
 app.get('/receive', makeHash, (req, res) => {
-  res.json({ shorturl: `https://stphn.xyz/${req.hash}` })
+  const name = req.query.name || req.hash
+  database.ref(`links/${name}`).set({
+    hash: name,
+    url: req.query.url
+  }, (data) => {
+    res.json({ shorturl: `https://${req.hostname}/${name}` })
+  });
+})
+
+app.get('/:hash', (req, res) => {
+  database.ref(`/links/${req.params.hash}`)
+    .once('value')
+    .then(snapshot => {
+      res.redirect(snapshot.val().url)
+    })
 })
 
 app.listen(3000, () => {
