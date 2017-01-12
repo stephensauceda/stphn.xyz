fc4f14dc2911b793f8f30e5b9e164a48e86ea7bc
Stephen Sauceda <contact@stephensauceda.com>
2017-01-11 22:04:16 -0500

    update env vars

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase'
 
 const config = {
-  apiKey: process.env.apikey,
-  authDomain: process.env.authdomain,
-  databaseURL: process.env.databaseurl,
-  storageBucket: process.env.storagebucket,
-  messagingSenderId: process.env.messagingsenderid
+  apiKey: process.env.stphn_apikey,
+  authDomain: process.env.stphn_authdomain,
+  databaseURL: process.env.stphn_databaseurl,
+  storageBucket: process.env.stphn_storagebucket,
+  messagingSenderId: process.env.stphn_messagingsenderid
 };
 
 firebase.initializeApp(config);
